2f6b373658cfccd6b884443bdac560f1437dd5d7
SnowSq <92370031+SnowSq@users.noreply.github.com>
2022-11-26 20:32:23 +0300

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,101 @@
+#Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+#и проверяет, является ли этот день выходным.
+
+#Пример:
+
+#- 6 -> да
+#- 7 -> да
+#- 1 -> нет
+
+n = int(input('Введите день недели'))
+if 6 <= n <= 7:
+print ('Да') 
+elif 0 < n < 6:
+print("Нет")
+else:
+print("Нет")
+
+
+#Напишите программу для. проверки истинности утверждения 
+#¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+#⋀ - and
+#⋁ - or
+#¬ - not
+
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
+
+#Напишите программу, которая принимает на вход координаты точки (X и Y), 
+#причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+#Пример:
+
+#- x=34; y=-30 -> 4
+#- x=2; y=4-> 1
+#- x=-34; y=-30 -> 3
+#Напишите программу, которая по заданному номеру четверти, 
+#показывает диапазон возможных координат точек в этой четверти (x и y).
+
+def inputKoord(x):
+    a = [0] * x
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = float(input(f"Введите {i+1} координату: "))
+                a[i] = number
+                is_OK = True
+                if a[i] == 0:
+                    is_OK = False
+                    print("{X и Y не должны быть равны 0}")
+            except ValueError:
+                print("Ошибка")
+    return a
+
+
+def checkCoordinates(xy):
+    text = 4
+    if xy[0] > 0 and xy[1] > 0:
+        text = 1
+    elif xy[0] < 0 and xy[1] > 0:
+        text = 2
+    elif xy[0] < 0 and xy[1] < 0:
+        text = 3
+    print(f"Точка находится в {text} четверти плоскости")
+
+
+koordinate = inputKoord(2)
+checkCoordinates(koordinate)
+
+
+#Напишите программу, которая принимает на вход координаты двух точек и 
+#находит расстояние между ними в 2D пространстве.
+
+#Пример:
+
+#- A (3,6); B (2,1) -> 5,09
+#- A (7,-5); B (1,-1) -> 7,21
+
+a1, b1 = map(int, input('Введите координаты первой точки: ').split())
+a2, b2 = map(int, input('Введите координаты второй точки: ').split())
+R = round(((a2 - a1)**2 + (b2 - b1)**2)**(1/2), 2)
+print(R)
